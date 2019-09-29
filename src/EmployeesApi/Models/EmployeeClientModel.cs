be017fe388596678f495fed6e085624c9f0ce21a
olga_gorbacheva <olgaklyuchevskaya00@gmail.com>
2019-09-29 22:07:33 +0300

    Добавлены комментарии

--- src/EmployeesApi/Models/EmployeeClientModel.cs
+++ src/EmployeesApi/Models/EmployeeClientModel.cs
@@ -1,12 +1,35 @@
 ﻿﻿namespace EmployeesApi.Models
 {
-    public class EmployeeClientModel
+/**
+ * Класс, отображающий модель данных сотрудника согласно представлению в базе данных
+ *
+ * @author Gorbacheva Olga
+ */
+ public class EmployeeClientModel
     {
+        /**
+         * Id сотрудника
+         */
         public int Id { get; set; }
+        /**
+         * Имя сотрудника
+         */
         public string Name { get; set; }
+        /**
+         * Фамилия сотрудника
+         */
         public string Surname { get; set; }
+        /**
+         * Телефон сотрудника
+         */
         public string Phone { get; set; }
+        /**
+         * Id компании сотрудника
+         */
         public int? CompanyId { get; set; }
+        /**
+         * Паспорт сотрудника
+         */
         public Passport Passport { get; set; }
     }
 
