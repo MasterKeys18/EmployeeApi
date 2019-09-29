2721b42475f9051c41d4e589022560d10e44b3ec
olga_gorbacheva <olgaklyuchevskaya00@gmail.com>
2019-09-29 20:16:11 +0300

    Произведен рефакторинг

--- src/EmployeesApi/Models/EmployeeDbModel.cs
+++ src/EmployeesApi/Models/EmployeeDbModel.cs
@@ -2,12 +2,33 @@
 {
     public class EmployeeDbModel
     {
+        /**
+         * Id сотрудника
+         */
         public int Id { get; set; }
+        /**
+         * Имя сотрудника
+         */
         public string Name { get; set; }
+        /**
+         * Фамилия сотрудника
+         */
         public string Surname { get; set; }
+        /**
+         * Телефон сотрудника
+         */
         public string Phone { get; set; }
+        /**
+         * Id компании сотрудника
+         */
         public int? CompanyId { get; set; }
+        /**
+         * Серия паспорта сотрудника
+         */
         public string Type { get; set; }
+        /**
+         * Номер паспорта сотрудника
+         */
         public string Number { get; set; }
     }
 }
